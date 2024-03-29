54e9c1184498f057e2db0f940e72e621393a3e03
Henrique Fonseca <henriquefons@hotmail.com>
2024-03-29 18:46:47 -0300

    Improve Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/images/logo-white.png";
-import profileDefault from "@/assets/images/logo-white.png";
+import profileDefault from "@/assets/images/profile.png";
+import { FaGoogle } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isMobileMenuHidden, setIsMobileMenuHidden] = useState(true);
+  const [isUserMenuHidden, setIsUserMenuHidden] = useState(true);
+  function toggleNavbar() {
+    setIsMobileMenuHidden(!isMobileMenuHidden);
+  }
+  function toggleUserMenu() {
+    setIsUserMenuHidden(!isUserMenuHidden);
+  }
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             <button
+              onClick={toggleNavbar}
               type="button"
               id="mobile-dropdown-button"
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -36,33 +49,33 @@ const Navbar = () => {
           </div>
 
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <a className="flex flex-shrink-0 items-center" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center" href="/">
               <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 PropertyPulse
               </span>
-            </a>
+            </Link>
             <div className="hidden md:ml-6 md:block">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <Link
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
-                </a>
-                <a
-                  href="/properties.html"
+                </Link>
+                <Link
+                  href="/properties"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Properties
-                </a>
-                <a
-                  href="/add-property.html"
+                </Link>
+                <Link
+                  href="/properties/create"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Property
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -70,14 +83,14 @@ const Navbar = () => {
           <div className="hidden md:block md:ml-6">
             <div className="flex items-center">
               <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                <i className="fa-brands fa-google text-white mr-2"></i>
+                <FaGoogle className="text-white mr-2" />
                 <span>Login or Register</span>
               </button>
             </div>
           </div>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-            <a href="messages.html" className="relative group">
+            <Link href="/messages" className="relative group">
               <button
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -102,10 +115,11 @@ const Navbar = () => {
               <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
                 2
               </span>
-            </a>
+            </Link>
             <div className="relative ml-3">
               <div>
                 <button
+                  onClick={toggleUserMenu}
                   type="button"
                   className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                   id="user-menu-button"
@@ -124,67 +138,68 @@ const Navbar = () => {
 
               <div
                 id="user-menu"
-                className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                  isUserMenuHidden ? "hidden" : ""
+                }`}
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="user-menu-button"
                 tabIndex="-1"
               >
-                <a
-                  href="/profile.html"
+                <Link
+                  href="/profile"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-0"
                 >
                   Your Profile
-                </a>
-                <a
-                  href="saved-properties.html"
+                </Link>
+                <Link
+                  href="/properties/saved"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Saved Properties
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <button
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Sign Out
-                </a>
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="hidden" id="mobile-menu">
+      <div className={[isMobileMenuHidden ? "hidden" : ""]} id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="/index.html"
+          <Link
+            href="/"
             className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Home
-          </a>
-          <a
-            href="/properties.html"
+          </Link>
+          <Link
+            href="/properties"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Properties
-          </a>
-          <a
-            href="/add-property.html"
+          </Link>
+          <Link
+            href="/properties/create"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Add Property
-          </a>
+          </Link>
           <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-            <i className="fa-brands fa-google mr-2"></i>
+            <FaGoogle className="text-white mr-2" />
             <span>Login or Register</span>
           </button>
         </div>
